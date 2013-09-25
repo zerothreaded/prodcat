7b512951f8b4bc24395d014ee6f15633fe03f78a
unknown <Ralphm@WH5000369.Adgency-Domain.Ad-Gency.com>
2013-09-25 17:05:39 +0100

    Completing first flow , added splitter, router and content transformers

--- src/main/java/com/williamhill/pds/services/FileChangedNotificationService.java
+++ src/main/java/com/williamhill/pds/services/FileChangedNotificationService.java
@@ -11,15 +11,25 @@ public class FileChangedNotificationService {
 
     public Message<String> handleFileChangedNotification(File file) {
         System.out.println("handleFileChangedNotification() called for file [" + file.getName() + "]");
+        // Extract the OXIREP level from the file name pattern
+        // TODO: Review this idea later - it smells wrong
+        String[] fileNameSections = file.getName().split("_");
+        String oxiReplevel = fileNameSections[1];
+       
+        // Convert file content to message payload
         String content;
 		try {
 			content = FileUtils.readFileToString(file);
 		} catch (IOException e) {
-			content = "Error reading file. Exception Message = " + e.getMessage();
+			content = "Error reading file. Exception Message = [" + e.getMessage() + "]";
 		}
 		
-		// Build message from File Content
-		Message<String> message = MessageBuilder.withPayload(content).setHeader("file.name", file.getName()).build();
+		// Build message from File Content and decorate headers
+		Message<String> message = MessageBuilder
+				.withPayload(content)
+				.setHeader("file.name", file.getName())
+				.setHeader("oxirep.level", oxiReplevel)
+				.build();
         return message;
     }
 }
